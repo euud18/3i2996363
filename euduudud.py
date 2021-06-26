4250e6f64029aaed19e39f213f4ba648b7828fd3
euud18 <86521550+euud18@users.noreply.github.com>
2021-06-26 15:43:58 +0300

    Add files via upload

---
+++ euduudud.py
@@ -0,0 +1,11 @@
+import request 
+print('1 - Проверить альбом по UPC\n2 - Проверить трек по upc')
+s1 = int(input ('Выбери вариант: '))
+if s1 == 1:
+  s11 = input ('Введи UPC: ')
+  URL = f'https://api.deezer.com/album/upc:{s11}'
+  HEADERS = {
+    'Accept': 'application/json, text/plain, */*'
+  }
+  req = request.get(URL, HEADERS).json()
+  print(req)
